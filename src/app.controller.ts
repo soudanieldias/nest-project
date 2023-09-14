e025ef6e089f37696d29fcc9685013697b6d9843
soudanieldias <danieldias.b99@hotmail.com>
2023-09-14 00:00:11 -0300

    feat: initial controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,31 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PostService } from './posts/post.service';
+import { UserService } from './users/user.service';
+import { User as UserModel, Post as PostModel } from '@prisma/client';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly postService: PostService,
+    private readonly userService: UserService,
+  ) {}
 
-  @Get()
+  @Get('/healthcheck')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  // USERS ROUTES
+  @Get('users/')
+  async getUsers(): Promise<UserModel[]> {
+    return this.userService.users({});
+  }
+
+  // POSTS ROUTES
+  @Get('posts/')
+  async getPosts(): Promise<PostModel[]> {
+    return this.postService.posts({});
   }
 }
