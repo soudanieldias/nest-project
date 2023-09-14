40d8e121e8d2763faa9bbaaf091b8f43fddc6aba
soudanieldias <danieldias.b99@hotmail.com>
2023-09-13 23:58:43 -0300

    feat: add dependency injections forn app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostService } from './posts/post.service';
+import { UserService } from './users/user.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController], // Controllers das Rotas de acesso a Dados
+  providers: [AppService, PostService, UserService, PrismaService], // Provedor de Dados que se conectam ao DB
 })
 export class AppModule {}
