71a9b1c3458f3ce9678a99c9a9cd9066331a4772
soudanieldias <danieldias.b99@hotmail.com>
2023-09-15 23:53:33 -0300

    feat: add swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger Config
+  const config = new DocumentBuilder()
+    .setTitle('API Rede Social')
+    .setDescription(
+      'Documentação criada como complemento da API desenvolvida para teste técnico.',
+    )
+    .setVersion('1.0')
+    .addTag('api')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  // Swagger UI Config
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
